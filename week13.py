d4cde57a6f9f8c0105fc2c55f2558d316cd75c9a
Gitprincesskerry <60557711+Gitprincesskerry@users.noreply.github.com>
2020-05-03 03:48:31 -0400

    Update week13.py

--- week13.py
+++ week13.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 # This is Kerry Rainford's Week 13 Assignment
 
-import re, sqlite3
-from flask import Flask, render_template, request, redirect, sessions, g, flash, url_for
+import sqlite3
+from flask import Flask, render_template, request, redirect, session, g, flash, url_for
+from markupsafe import escape
 app = Flask(__name__)
-#app.secretkey = 'd\xe9X\x00\xbe~Uq\xebX\xae\x81\x1fs\t\xb4\x99\xa3\x87\xe6.\xd1_'
+app.secret_key = 'kerrywillnotsay'
 
-secretkey= '#d\xe9X\x00\xbe~Uq\xebX\xae\x81\x1fs\t\xb4\x99\xa3\x87\xe6.\xd1_'
 username = 'admin'
 password = 'password'
 hw13database = 'hw13.db'
@@ -24,9 +24,10 @@ def login():
     pword = request.form['password'].strip()
 
     if uname == username and pword == password:
+        session['logged_in'] = True
         return redirect(url_for('dashboard'))
     else:
-        print('Invalid username or password')
+        flash = "Invalid username or password. Try again!"
         return redirect(url_for('home'))
 
 def connect():
@@ -45,14 +46,19 @@ def teardown_request(exception):
 
 @app.route('/dashboard')
 def dashboard():
-    student_table = g.db.execute("SELECT id, first_name, last_name FROM student").fetchall()
-    quizzes = g.db.execute("select id, subject, question_total, date_quiz_taken from quiz").fetchall()
-    return render_template('dashboard.html', students=student_table, quizzes=quizzes)
-
+    if session['logged_in'] is not True:
+        return redirect('/login')
+    else:
+        student_table = g.db.execute("SELECT id, first_name, last_name FROM student").fetchall()
+        quizzes = g.db.execute("select id, subject, question_total, date_quiz_taken from quiz").fetchall()
+        return render_template('dashboard.html', students=student_table, quizzes=quizzes)
 
 @app.route('/student/add', methods = ['GET','POST'])
 def addstudent():
-    if request.method == 'GET':
+    if session['logged_in'] is not True:
+        return redirect('/login')
+
+    elif request.method == 'GET':
         return render_template('addstudent.html')
 
     elif request.method == 'POST':
@@ -63,7 +69,10 @@ def addstudent():
 
 @app.route('/quiz/add', methods = ['GET','POST'])
 def addquiz():
-    if request.method == 'GET':
+    if session['logged_in'] is not True:
+        return redirect('/login')
+
+    elif request.method == 'GET':
         return render_template('addquiz.html')
 
     elif request.method == 'POST':
@@ -72,9 +81,40 @@ def addquiz():
         g.db.commit()
         return redirect(url_for('dashboard'))
 
-@app.route('/results/add')
+@app.route('/student/<id>', methods = ['GET'])
+def quizresults(id):
+    if session['logged_in'] is not True:
+        return redirect('/login')
+
+    else:
+
+        cursor = g.db.cursor()
+        cursor.execute("SELECT count(*) FROM studentresults WHERE student_id =" + id)
+        count=cursor.fetchone()
+        #cursor.commit()
+
+        id_score = g.db.execute("SELECT student_id, score FROM studentresults WHERE student_id =" + id).fetchall()
+
+        return render_template('quizresults.html', id_score=id_score, count=count)
+
+@app.route('/results/add', methods = ['GET','POST'])
 def addresults():
-    return render_template('quizresults.html')
+    if session['logged_in'] is not True:
+        return redirect('/login')
+
+    elif request.method == 'GET':
+        id_score = g.db.execute("SELECT student_id, score FROM studentresults").fetchall()
+        id_quiz = g.db.execute("SELECT id, subject FROM quiz").fetchall()
+
+        return render_template('addscore.html', id_score=id_score,id_quiz=id_quiz)
+
+    elif request.method == 'POST':
+        print("test")
+        g.db.execute("INSERT INTO studentresults (student_id, quiz_id, score) VALUES(?, ?, ?)",
+                    [request.form['StudentID'],request.form['quizname'],request.form['Score']])
+        g.db.commit()
+
+        return redirect(url_for('dashboard'))
 
 if __name__ == "__main__":
     app.run()
